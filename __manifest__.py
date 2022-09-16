6eaf90d323fbbf156ed780c493ea62415118d60c
Nelson V <nelson.avelez@hotmail.com>
2022-09-16 17:58:34 -0500

    Dependencias

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','stock','payment_report_co'],
 
     # always loaded
     'data': [
