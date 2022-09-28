d4d67871374811d31cc5314406e1786480405fc6
Gavii <agaviriat@gmail.com>
2022-09-28 10:22:22 -0500

    [ADD] New model RequiredProcess

--- models/l10n_stock_picking.py
+++ models/l10n_stock_picking.py
@@ -22,8 +22,16 @@ class PickingInfoExtended(models.Model):
     x_transportadora = fields.Char('Transportadora')
     x_cedula = fields.Char('Cédula')
     x_celular = fields.Char('Celular')
+    required_process_ids = fields.Many2many('stock.required_process', "Requirió procesos logísticos adicionales")
 
 class StockMoveExtended(models.Model):
     _inherit = "stock.move"
 
     x_estibas = fields.Integer('Estibas')
+
+class RequiredProcess(models.AbstractModel):
+
+    _name = 'stock.required_process'
+
+    name = fields.Char()
+
